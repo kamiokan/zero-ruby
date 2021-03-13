d4cb46f1aa0ac1d47a1f250e65227f0a0dfa7bd5
kamiokan <spherenov@gmail.com>
2021-03-13 14:30:41 +0900

    日報用のスクリプト

--- practice5-14.rb
+++ practice5-14.rb
@@ -4,3 +4,7 @@ p %w[aya achi Tama].map { |str| str.downcase }.sort
 
 # another way
 p %w[aya achi Tama].map(&:downcase).sort
+
+jujutsu_kousen_tokyo_members = %w[虎杖悠仁 伏黒恵 釘崎野薔薇 乙骨憂太 禪院真希 狗巻棘 	パンダ 秤金次]
+puts jujutsu_kousen_tokyo_members.first # => 虎杖悠仁
+puts jujutsu_kousen_tokyo_members.last # => 秤金次
