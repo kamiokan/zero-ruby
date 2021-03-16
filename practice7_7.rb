7840e90291254317ac75bd175ec748fd14e240dc
kamiokan <spherenov@gmail.com>
2021-03-17 08:29:12 +0900

    chapter 7 答え合わせ

--- practice7_7.rb
+++ practice7_7.rb
@@ -1,26 +1,9 @@
 # frozen_string_literal: true
 
 def price(item:, size:)
-  case item
-  when 'コーヒー'
-    case size
-    when 'ショート'
-      300 + 0
-    when 'トール'
-      300 + 50
-    when 'ベンティ'
-      300 + 100
-    end
-  when 'カフェラテ'
-    case size
-    when 'ショート'
-      400 + 0
-    when 'トール'
-      400 + 50
-    when 'ベンティ'
-      400 + 100
-    end
-  end
+  items = { 'コーヒー' => 300, 'カフェラテ' => 400 }
+  sizes = { 'ショート' => 0, 'トール' => 50, 'ベンティ' => 100 }
+  items[item] + sizes[size]
 end
 
 puts price(item: 'コーヒー', size: 'ショート')
